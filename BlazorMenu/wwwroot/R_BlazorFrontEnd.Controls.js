c7c0af5f17c270629db83a28203cc234b2727d35
siholsihol <christiantosihol@hotmail.com>
2023-06-12 00:57:21 +0700

    add js

---
+++ BlazorMenu/wwwroot/R_BlazorFrontEnd.Controls.js
@@ -0,0 +1,40 @@
+// This is a JavaScript module that is loaded on demand. It can export any number of
+// functions, and may import other JavaScript modules if required.
+
+export function showPrompt(message) {
+  return prompt(message, 'Type anything here');
+}
+
+export function selectText(tbId) {
+    var tb = document.querySelector("#" + tbId);
+
+    if (tb.select) {
+        tb.select();
+    }
+}
+
+export function setValueById(id, value) {
+    document.getElementById(id).value = value;
+}
+
+export function scrollToSelectedRow(gridSelector) {
+    var gridWrapper = document.querySelector(gridSelector);
+    if (gridWrapper) {
+        var selectedRow = gridWrapper.querySelector("tr.k-state-selected");
+        if (selectedRow) {
+            selectedRow.scrollIntoView();
+        }
+    }
+}
+
+export async function downloadFileFromStream(fileName, contentStreamReference) {
+    const arrayBuffer = await contentStreamReference.arrayBuffer();
+    const blob = new Blob([arrayBuffer]);
+    const url = URL.createObjectURL(blob);
+    const anchorElement = document.createElement('a');
+    anchorElement.href = url;
+    anchorElement.download = fileName ?? '';
+    anchorElement.click();
+    anchorElement.remove();
+    URL.revokeObjectURL(URL);
+}
