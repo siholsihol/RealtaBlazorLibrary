78d0ee9114d09bbfa21501b874e9030c70227767
siholsihol <christiantosihol@hotmail.com>
2024-10-08 11:29:35 +0700

    bug fix locking navigator 20241008

--- BlazorMenu/Routing/Package.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BlazorMenu.Routing
-{
-    public class Package
-    {
-        public string FileName { get; set; }
-        public byte[] Content { get; set; }
-        public bool IsReference { get; set; }
-    }
-}
