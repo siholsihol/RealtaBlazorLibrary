3386b7ba7389134ea0116fbd75d84aa653bf0087
siholsihol <christiantosihol@hotmail.com>
2025-06-20 13:32:27 +0700

    initial

--- BlazorMenu/Program.cs
+++ BlazorMenu/Program.cs
@@ -6,6 +6,8 @@ using R_BlazorFrontEnd.Controls.Extensions;
 using R_BlazorFrontEnd.FileConverter;
 using R_BlazorFrontEnd.Helpers;
 using R_BlazorFrontEnd.Interfaces;
+using R_BlazorFrontEnd.Report;
+using R_BlazorFrontEnd.Tenant.Extensions;
 using R_BlazorStartup;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -14,25 +16,24 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.R_AddBlazorFrontEndControls();
 
-builder.R_RegisterBlazorServices(option =>
+builder.R_RegisterBlazorServices(opt =>
 {
-    if (!builder.HostEnvironment.IsDevelopment())
-        option.R_WithMultiTenant();
+    opt.R_WithMultiTenant();
 });
 
 builder.Services.R_AddBlazorMenuServices();
 
 builder.Services.AddTransient<R_IFileConverter, R_FileConverter>();
-//builder.Services.AddTransient<R_IReport, R_ReportService>();
+builder.Services.AddTransient<R_IReport, R_TenantReportService>();
 builder.Services.AddSingleton<R_IFileDownloader, R_FileDownloader>();
 
 Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", builder.HostEnvironment.Environment);
 
-//builder.Services.AddMultiTenantancy();
+builder.Services.AddMultiTenantancy();
 
 var host = builder.Build();
 
-//host.Services.AddServiceProviderToTenantRoutes();
+host.Services.AddServiceProviderToTenantRoutes();
 
 host.R_SetupBlazorService();
 
