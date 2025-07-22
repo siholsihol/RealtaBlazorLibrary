6a88c63aa3e20a6e51785eb1ce86ab1d184f3c91
siholsihol <christiantosihol@hotmail.com>
2025-07-22 11:28:58 +0700

    bug fix loading di r_print button dan Bug Fix Time Picker style in Dark Theme

--- BlazorMenu/wwwroot/R_BlazorFrontEnd.Controls.js
+++ BlazorMenu/wwwroot/R_BlazorFrontEnd.Controls.js
@@ -13,15 +13,23 @@ export function selectText(tbId) {
 }
 
 export async function downloadFileFromStream(fileName, contentStreamReference) {
-    const arrayBuffer = await contentStreamReference.arrayBuffer();
-    const blob = new Blob([arrayBuffer]);
-    const url = URL.createObjectURL(blob);
-    const anchorElement = document.createElement('a');
-    anchorElement.href = url;
-    anchorElement.download = fileName ?? '';
-    anchorElement.click();
-    anchorElement.remove();
-    URL.revokeObjectURL(URL);
+    try {
+        const arrayBuffer = await contentStreamReference.arrayBuffer();
+        const blob = new Blob([arrayBuffer]);
+        const url = URL.createObjectURL(blob);
+
+        const anchorElement = document.createElement('a');
+        anchorElement.href = url;
+        anchorElement.download = fileName ?? '';
+        document.body.appendChild(anchorElement);
+        anchorElement.click();
+        anchorElement.remove();
+
+        URL.revokeObjectURL(url);
+    } catch (err) {
+        console.error("JS error in downloadFileFromStream", err);
+        throw err;
+    }
 }
 
 export function showBootstrapToast() {
